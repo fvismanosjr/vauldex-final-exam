a5ccb27572cfd6df07a55189f5c4dfaaa69d1be2
Fermin Vismanos Jr <ferminvismanosjr@gmail.com>
2025-12-10 08:33:18 +0800

    feat: initiliaze backend

---
+++ backend/src/main/kotlin/com/example/final_exam/FinalExamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.final_exam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinalExamApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinalExamApplication>(*args)
+}
